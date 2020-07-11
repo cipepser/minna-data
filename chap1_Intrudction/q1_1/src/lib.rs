e809d9699e9f4e48e6e0862bd8203ea291933646
cipepser <respepic@gmail.com>
2020-07-11 21:28:29 +0900

    solved

--- chap1_Intrudction/q1_1/src/lib.rs
+++ chap1_Intrudction/q1_1/src/lib.rs
@@ -2,3 +2,4 @@ mod q1_1_1;
 mod q1_1_2;
 mod q1_1_3;
 mod q1_1_4;
+mod q1_1_5;
---
+++ chap1_Intrudction/q1_1/src/q1_1_5.rs
@@ -0,0 +1,34 @@
+use std::{
+    fs::File,
+    io::{
+        self,
+        prelude::*,
+        BufReader,
+        BufWriter,
+    },
+    collections::HashSet
+};
+
+#[test]
+fn q1_1_4() {
+    let f = File::open("./data4.txt").unwrap();
+    let f = BufReader::new(f);
+
+    let stdout = io::stdout();
+    let mut stdout = BufWriter::new(stdout.lock());
+    let mut set: HashSet<String> = HashSet::new();
+
+    for line in f.lines() {
+        let s = &line.unwrap();
+        match set.get(s) {
+            None =>  {
+                set.insert(s.to_string());
+            },
+            Some(_) => {
+                writeln!(stdout, "{:?}", s).unwrap();
+            },
+        }
+    }
+    stdout.flush().unwrap();
+}
+

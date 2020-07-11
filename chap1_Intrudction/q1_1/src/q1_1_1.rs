e9bcb51d07ba56f898a17e007d1a0abd40e46dd2
cipepser <respepic@gmail.com>
2020-07-11 18:46:38 +0900

    use proconio

--- chap1_Intrudction/q1_1/src/q1_1_1.rs
+++ chap1_Intrudction/q1_1/src/q1_1_1.rs
@@ -1,4 +1,36 @@
+use proconio::{
+    input,
+    source::{
+        line::LineSource,
+        once::OnceSource,
+    },
+    marker::Bytes,
+};
+use std::{
+    fs::File,
+    io::{self, BufReader, BufWriter, Write as _},
+};
+
 #[test]
+#[proconio::fastout]
 fn q1_1_1() {
-    assert!()
+    let f = File::open("./data.txt").unwrap();
+    let mut input_steam = BufReader::new(f);
+    let source = LineSource::new(input_steam);
+
+    // let stdout = io::stdout();
+    // let mut stdout = BufWriter::new(stdout.lock());
+
+    input! {
+        from source,
+        lines: [Bytes; 30],
+    }
+    println!("{:?}", lines);
+
+    for line in lines.iter().rev() {
+        let s = String::from_utf8(line.to_vec()).unwrap();
+        println!("{}", s);
+        // writeln!(stdout, "{:?}", line).unwrap();
+    }
+    // stdout.flush().unwrap();
 }
